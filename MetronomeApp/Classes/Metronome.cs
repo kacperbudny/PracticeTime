0a337e35f9f1f6d090c81b07995f518523b1a42a
Kacper Budny <kacperbudny@hotmail.com>
2020-11-13 18:39:55 +0100

    Code cleanup and some setting tempo basics

---
+++ MetronomeApp/Classes/Metronome.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using MetronomeApp.Properties;
+
+namespace MetronomeApp.Classes
+{
+    public class Metronome
+    {
+        List<DateTime> times = new List<DateTime>();
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        SoundPlayer metronomeSound = new SoundPlayer(Resources.metronome);
+
+        public bool IsMetronomePlaying { get; set; }
+        private int Tick;
+        private int Sleep;
+
+        public Metronome()
+        {
+            Tick = 500;
+        }
+        
+        public async Task Run()
+        {
+            Sleep = Tick - 20;
+            long OldElapsedMilliseconds = 0;
+            IsMetronomePlaying = true;
+
+            sw.Start();
+
+            while (sw.IsRunning && IsMetronomePlaying)
+            {
+                long ElapsedMilliseconds = sw.ElapsedMilliseconds;
+                long mod = (ElapsedMilliseconds % Tick);
+
+                if (OldElapsedMilliseconds != ElapsedMilliseconds && (mod == 0 || ElapsedMilliseconds > Tick))
+                {
+                    metronomeSound.Play();
+                    times.Add(DateTime.Now);
+
+                    //Restarting the counter
+                    OldElapsedMilliseconds = ElapsedMilliseconds;
+                    OldElapsedMilliseconds = 0;
+                    sw.Reset();
+                    sw.Start();
+
+                    await Task.Delay(Sleep);
+                }
+            }
+
+            if (!IsMetronomePlaying)
+            {
+                SaveTempoList();
+            }
+        }
+
+        public void Stop()
+        {
+            IsMetronomePlaying = false;
+        }
+
+        public void SetTempo(int tempo)
+        {
+            Tick = tempo;
+            Sleep = Tick - 20;
+        }
+
+        private void SaveTempoList()
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\czasy zmiana tempa.txt"))
+            {
+                foreach (DateTime time in times)
+                {
+                    file.WriteLine(time.ToString("ss.fff"));
+                }
+            }
+        }
+    }
+}
--- MetronomeApp/MainWindow.xaml.cs
+++ MetronomeApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetronomeApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -25,124 +26,34 @@ namespace MetronomeApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\kapib\source\repos\MetronomeApp\MetronomeApp\Resources\metronome.wav");
-        bool isMetronomeOn = false;
-        //System.Timers.Timer timer = new System.Timers.Timer();
-        List<string> czasy;
-        RealTimeTimerTest metronome = new RealTimeTimerTest();
+        Metronome metronome = new Metronome();
 
 
         public MainWindow()
         {
             InitializeComponent();
-            //timer.Elapsed += Timer_Elapsed;
-            //timer.Interval += 500;
-            czasy = new List<string>();
         }
-
-        //private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        //{
-        //    simpleSound.Play();
-        //    czasy.Add(DateTime.Now.ToString("ss.ffff"));
-        //}
 
         private async void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            if (isMetronomeOn == false)
+            if (metronome.IsMetronomePlaying == false)
             {
                 StartButton.Content = "STOP";
-                isMetronomeOn = true;
-                //await StartMetronomeAsync();
-                //timer.Start();
+                metronome.IsMetronomePlaying = true;
                 await metronome.Run();
 
             }
             else
             {
-                isMetronomeOn = false;
-                StartButton.Content = "START";
-                //timer.Stop();
                 metronome.IsMetronomePlaying = false;
-
-                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\kapib\Desktop\\czasy async bez dzwieku.txt"))
-                {
-                    foreach (string czas in czasy)
-                    {
-
-                        file.WriteLine(czas);
-
-                    }
-                }
+                metronome.Stop();
+                StartButton.Content = "START";
             }
         }
 
-        private async Task StartMetronomeAsync()
+        private void ChangeTempoButton_Click(object sender, RoutedEventArgs e)
         {
-            while (isMetronomeOn)
-            {
-                simpleSound.Play();
-                czasy.Add(DateTime.Now.ToString("ss.ffff"));
-                await Task.Delay(500);
-            }
-        }
-    }
-}
-
-public class RealTimeTimerTest
-{
-    List<DateTime> lst = new List<DateTime>();
-    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-    SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\kapib\source\repos\MetronomeApp\MetronomeApp\Resources\metronome.wav");
-    public bool IsMetronomePlaying { get; set; }
-
-    public async Task Run()
-    {
-        int Tick = 500;
-        int Sleep = Tick - 20;
-        long OldElapsedMilliseconds = 0;
-        IsMetronomePlaying = true;
-        sw.Start();
-
-        while (sw.IsRunning && IsMetronomePlaying)
-        {
-            long ElapsedMilliseconds = sw.ElapsedMilliseconds;
-            long mod = (ElapsedMilliseconds % Tick);
-
-            if (OldElapsedMilliseconds != ElapsedMilliseconds && (mod == 0 || ElapsedMilliseconds > Tick))
-            {
-
-                //-----------------Do here whatever you want to do--------------Start
-                simpleSound.Play();
-                lst.Add(DateTime.Now);
-
-                //-----------------Do here whatever you want to do--------------End
-
-                //-----------------Restart----------------Start
-                OldElapsedMilliseconds = ElapsedMilliseconds;
-                OldElapsedMilliseconds = 0;
-                sw.Reset();
-                sw.Start();
-
-                await Task.Delay(Sleep);
-                //-----------------Restart----------------End
-            }
-
-            //------------Must define some condition to break the loop here-----------Start
-
-            //if (lst.Count > 10)
-            //{
-            //    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\kapib\Desktop\\czasy async bez dzwieku.txt"))
-            //    {
-            //        foreach (DateTime czas in lst)
-            //        {
-
-            //            file.WriteLine(czas.ToString("ss.ffff"));
-
-            //        }
-            //    }
-            //    break;
-            //}
-            //-------------Must define some condition to break the loop here-----------End
+            metronome.SetTempo(1000);
         }
     }
 }
